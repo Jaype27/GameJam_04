80c8aaf741857fca3b6c32b5f0b21147ac3510da
Tristin Bridgeman <bridgemantristin@gmail.com>
2018-02-25 04:47:19 -0500

    Re added Player Script

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,15 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
-
-	
+ 
+	public float m_rotateSpeed = 10.0f; 
+	public float m_moveSpeed = 10.0f;
+	private Rigidbody2D m_rb;
+ 	
 	void Start () {
 
-        GetComponent<Rigidbody>();
-		
+       GetComponent<Rigidbody>();
+	
 	}
+	void Awake () {
+		m_rb = GetComponent<Rigidbody2D>(); 
+ 	}
+ 
+ 	void Update () {
+		
+		if (Input.GetAxis("Vertical") != 0) {
+			m_rb.velocity = Input.GetAxis("Vertical") * transform.up * -m_moveSpeed;
+		}
 
-	void Update () {
-		
-	}
+		if (Input.GetAxis("Horizontal") != 0) {
+			transform.Rotate(0, 0, -m_rotateSpeed * Time.deltaTime * Input.GetAxis("Horizontal"));
+		}
+ 	}
+	
+	 
 }
