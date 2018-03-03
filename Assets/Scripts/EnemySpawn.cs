5ae0b8c66b593152afa6f732387b76b039c4ef46
Mark Parsikhian <Malooka5432@hotmail.ca>
2018-03-02 23:07:59 -0500

    yolo

---
+++ Assets/Scripts/EnemySpawn.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawn : MonoBehaviour {
+	public GameObject enemy;
+	public Transform enemyPos;
+	private float repeatRate = 5.0f;
+
+	void Start () {
+		
+	}
+	
+	void OnTriggerEnter (Collider other) 
+	{
+		if (other.gameObject.tag == "Player")
+		{
+			InvokeRepeating("EnemySpawner", 0.5f, repeatRate);
+			Destroy(gameObject, 11);
+			gameObject.GetComponent<BoxCollider>().enabled = false;
+		}
+		
+	}
+
+	void EnemySpawner()
+	{
+		Instantiate(enemy, enemyPos.position, enemyPos.rotation);
+	}
+}
